df1e158a8646664149ed678116bf9d8f0c70fdc7
Garulf <535299+Garulf@users.noreply.github.com>
2022-01-09 14:58:59 -0500

    init

---
+++ plugin/main.py
@@ -0,0 +1,114 @@
+import webbrowser
+from pathlib import Path
+from difflib import SequenceMatcher as SM
+
+from flox import Flox, ICON_SETTINGS
+import playnite as pn
+
+
+SOURCE_FILTER = '#'
+INSTALL_FILTER = '@'
+SCORE_CUTOFF = 10
+PLUGIN_URI = 'playnite://playnite/installaddon/FlowLauncherExporter'
+
+def match(query, text):
+    return int(SM(
+                lambda x: x == " ", 
+                query.lower(), 
+                text.lower()).ratio() * 100
+            )
+
+class Playnite(Flox):
+
+    def load_settings(self):
+        self.playnite_path = self.settings.setdefault('playnite_path', str(pn.DATA_FOLDER))
+        if self.settings.get('playnite_path') == '':
+            self.playnite_path = str(pn.DATA_FOLDER)
+            self.settings['playnite_path'] = str(pn.DATA_FOLDER)
+        if not Path(self.playnite_path).exists():
+            self.add_item(
+                title='Library file not found!',
+                subtitle="Please set the path to Playnite\'s data directory in settings.",
+                icon=ICON_SETTINGS,
+                method=self.open_setting_dialog
+            )
+            self.add_item(
+                title='Install FlowLauncherExporter plugin.',
+                subtitle='FlowLauncherExporter plugin is required to use this plugin.',
+                icon='',
+                method=self.uri,
+                parameters=[PLUGIN_URI]
+
+            )
+
+    def main_search(self, query):
+        for game in self.games:
+            score = match(query, game.name)
+            if score >= SCORE_CUTOFF or query == '':
+                if game.is_installed:
+                    subtitle = game.install_directory
+                    uri = game.start_uri
+                else:
+                    subtitle = 'Not Installed'
+                    uri = game.show_uri
+                self.add_item(
+                    title=game.name,
+                    subtitle=f'{game.source["Name"]}: {subtitle}',
+                    icon=str(game.icon_path),
+                    method=self.uri,
+                    parameters=[uri],
+                    context=[game.show_uri],
+                    score=score
+                )
+
+    def source_filter(self, query):
+        if query == '':
+            sources = set([game.source for game in self.games])
+            for source in sources:
+                _ = self.add_item(
+                    title=f'{SOURCE_FILTER}{source}',
+                    subtitle='Filter by source.',
+                    icon='',
+                    method=self.change_query,
+                    dont_hide=True,
+                )
+                _['JsonRPCAction']['Parameters'] = [_['AutoCompleteText']]
+            return
+        self.games = [game for game in self.games if query.lower() in game.source.lower()]
+
+    def install_filter(self, query):
+        self.games = [game for game in self.games if not game.is_installed]
+
+    def query(self, query):
+        self.load_settings()
+        try:
+            self.games = pn.import_games(self.playnite_path)
+        except FileNotFoundError:
+
+            return
+        if query.startswith(SOURCE_FILTER):
+            query = query[len(SOURCE_FILTER):]
+            self.source_filter(query)
+        if query.startswith(INSTALL_FILTER):
+            query = query[len(SOURCE_FILTER):]
+            self.install_filter(query)
+        self.main_search(query)
+        
+
+    def context_menu(self, data):
+        show_uri = data[0]
+        icon = str(self.icon if Path(self.icon).is_absolute() else Path(self.plugindir, self.icon))
+        self.logger.warning(icon)
+        self.add_item(
+            title='Open in Playnite',
+            subtitle='Shows Game in Playnite library.',
+            icon=icon,
+            method=self.uri,
+            parameters=[show_uri],
+        )
+
+    def uri(self, uri):
+        webbrowser.open(uri)
+
+if __name__ == "__main__":
+    Playnite()
---
+++ plugin/playnite.py
@@ -0,0 +1,63 @@
+import json
+from pathlib import Path
+import os
+import webbrowser
+
+
+SCRIPT_NAME = 'FlowLauncher_Exporter'
+DATA_FOLDER = Path(os.getenv('APPDATA'), 'Playnite')
+PLUGIN_NAME = 'FlowLauncher_Exporter'
+
+
+def import_games(data_folder=DATA_FOLDER, file_name='library.json'):
+    library_file = Path(data_folder, 'ExtensionsData', SCRIPT_NAME, file_name)
+    games = []
+    with open(library_file, encoding='utf-8-sig') as f:
+        data = json.load(f)
+        for game in data:
+            # print(game)
+            games.append(Game(data_folder, game))
+    return games
+
+def camel_to_snake(text):
+    return ''.join(['_'+char.lower() if char.isupper() else char for char in text]).lstrip('_')
+
+class Game(object):
+
+    def __init__(self, data_folder, data) -> None:
+        self.data_folder = data_folder
+        self.data = data
+        for key, value in data.items():
+            if value == 'True':
+                value = True
+            elif value == 'False':
+                value = False
+            setattr(self, camel_to_snake(key).lower(), value)
+
+    @property
+    def start_uri(self):
+        return f'playnite://playnite/start/{self.id}'
+
+    @property
+    def show_uri(self):
+        return f'playnite://playnite/showgame/{self.id}'
+
+    @property
+    def icon_path(self):
+        if self.icon:
+            path = Path(self.data_folder, 'library', 'files', self.icon)
+            if path.is_file():
+                return path
+        return ''
+
+    def start(self):
+        webbrowser.open(self.uri)
+
+    def show_game(self):
+        webbrowser.open(self.show_uri)
+
+
+if __name__ == "__main__":
+    games = import_games()
+    for game in games:
+        print(game.icon_path)
